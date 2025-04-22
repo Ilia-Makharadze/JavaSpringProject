5579783084b285bcb7fa4b9fd13fb054249e309c
ilia's hub <120248378+Ilia-Makharadze@users.noreply.github.com>
2025-04-22 21:04:45 +0400

    add book api

--- src/main/java/ge/tsu/demo/controller/BookController.java
+++ src/main/java/ge/tsu/demo/controller/BookController.java
@@ -1,8 +1,12 @@
-package ge.tsu.demo;
+package ge.tsu.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpMethod;
 
 import java.util.List;
 import java.util.Map;
@@ -19,5 +23,21 @@ public class BookController {
         );
         model.addAttribute("books", books);
         return "index";
+    }
+
+    @GetMapping("/search")
+    public String searchBooks(Model model) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<List<Map<String, String>>> response = restTemplate.exchange(
+                "https://678e2d7ba64c82aeb11f5a99.mockapi.io/food/api/booksApi",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {}
+        );
+
+        List<Map<String, String>> books = response.getBody();
+        model.addAttribute("books", books);
+        return "index"; // იგივე HTML ტემპლეთი
     }
 }
