4d58428bfc6f44798c64053b2ed345e362021bd9
ilia's hub <120248378+Ilia-Makharadze@users.noreply.github.com>
2025-04-22 18:42:38 +0400

    first commit

---
+++ src/test/java/ge/tsu/demo/BookWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.tsu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
